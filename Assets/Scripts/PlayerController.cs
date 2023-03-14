fc092232b28cc2e1ec2aedfbd3d1eb120bc04f0e
Ashish Verma <ashv6561@gmail.com>
2023-03-14 14:44:19 +0530

    Add Basic Player Movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,53 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     BoxCollider2D bc2d;
-    float speed = 2f;
-    Vector2 direction = Vector2.right;
+    Vector2 direction = new Vector2(0, 0);
+    float tileSize = 2f;
+    bool isPlayerMoving = false;
+
     private void Awake() {
         rb2d = GetComponent<Rigidbody2D>();
         bc2d = GetComponent<BoxCollider2D>();
     }
 
-    private void FixedUpdate() {
-        
 
+    private void Update() {
+        UpdatePlayerDirection();
+        if (!isPlayerMoving)
+            direction = new Vector2(0, 0);
     }
-    
+
+    void UpdatePlayerDirection() {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !isPlayerMoving) {
+            isPlayerMoving = true;
+            direction = Vector2.up;
+            StartCoroutine(GoInDirection());
+        } else if (Input.GetKeyDown(KeyCode.DownArrow) && !isPlayerMoving) {
+            isPlayerMoving = true;
+            direction = Vector2.down;
+            StartCoroutine(GoInDirection());
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) && !isPlayerMoving) {
+            isPlayerMoving = true;
+            direction = Vector2.left;
+            StartCoroutine(GoInDirection());
+        } else if (Input.GetKeyDown(KeyCode.RightArrow) && !isPlayerMoving) {
+            isPlayerMoving = true;
+            direction = Vector2.right;
+            StartCoroutine(GoInDirection());
+        }
+    }
+
+
+    IEnumerator GoInDirection() {
+        yield return new WaitForSeconds(1f);
+        direction = new Vector2(0, 0);
+        isPlayerMoving = false;
+    }
+
+    private void FixedUpdate() {
+        Vector3 position = transform.position;
+        position.x = Mathf.Round(position.x + direction.x);
+        position.y = Mathf.Round(position.y + direction.y);
+        transform.position = position;
+    }
 }
