8d4fb6baaff41c71068e3ec261053b5f65f09bf1
Ashish Verma <ashv6561@gmail.com>
2023-03-15 10:31:18 +0530

    Added Gravity Disabling - Enabling UI.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class LevelManager : MonoBehaviour
     [SerializeField] GameObject blueLevelComplete;
     [SerializeField] Rigidbody2D greenPlayerRB;
     [SerializeField] Rigidbody2D bluePlayerRB;
+    [SerializeField] Image gravityText;
     Transform greenPlayer;
     Transform bluePlayer;
     public bool isLevelCompleted = false;
@@ -26,11 +29,13 @@ public class LevelManager : MonoBehaviour
 
     IEnumerator EnableAndDisableGravity() {
         while (!isLevelCompleted) {
-            greenPlayerRB.gravityScale = 3;
-            bluePlayerRB.gravityScale = 3;
+            greenPlayerRB.gravityScale = 3f;
+            bluePlayerRB.gravityScale = 3f;
+            gravityText.color = Color.green;
             yield return new WaitForSeconds(Constants.DISABLE_ENABLE_GRAVITY_INTERVAL);
-            greenPlayerRB.gravityScale = 0;
-            bluePlayerRB.gravityScale = 0;
+            gravityText.color = Color.grey;
+            greenPlayerRB.gravityScale = 0f;
+            bluePlayerRB.gravityScale = 0f;
             yield return new WaitForSeconds(Constants.DISABLE_ENABLE_GRAVITY_INTERVAL);
         }
     }
