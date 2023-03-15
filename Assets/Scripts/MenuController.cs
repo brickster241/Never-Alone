1673216b8fd825fa94943fefee280e64e1ec410e
Ashish Verma <ashv6561@gmail.com>
2023-03-15 16:05:52 +0530

    Added Audio to Gameplay.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,28 +9,32 @@ public class MenuController : MonoBehaviour
     [SerializeField] GameObject HowToPlayUI;
     [SerializeField] GameObject LevelSelectUI;
 
-
     public void DisplayHowToPlayUI() {
+        GameplayManager.Instance.PlayAudio(AudioType.BUTTON_CLICK);
         StartingScreenUI.SetActive(false);
         HowToPlayUI.SetActive(true);
     }
 
     public void DisplayLevelSelectUI() {
+        GameplayManager.Instance.PlayAudio(AudioType.BUTTON_CLICK);
         StartingScreenUI.SetActive(false);
         LevelSelectUI.SetActive(true);
     }
 
     public void BackToStartingScreenUI() {
+        GameplayManager.Instance.PlayAudio(AudioType.BUTTON_CLICK);
         HowToPlayUI.SetActive(false);
         LevelSelectUI.SetActive(false);
         StartingScreenUI.SetActive(true);
     }
 
     public void QuitApplication() {
+        GameplayManager.Instance.PlayAudio(AudioType.BUTTON_CLICK);
         Application.Quit();
     }
 
     public void LoadLevel(int level) {
+        GameplayManager.Instance.PlayAudio(AudioType.BUTTON_CLICK);
         SceneManager.LoadScene(level);
     }
 }
